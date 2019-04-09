1206bf3fa9c2c2e6a74d06547491d7849cdeb7a0
VB Easy-SCS <45724928+raj-smartclean@users.noreply.github.com>
2019-04-09 10:59:19 +0530

    Update config.php
    remove database connection error

--- repslyapi/config.php
+++ repslyapi/config.php
@@ -1,9 +1,9 @@
 <?php
 set_time_limit ( 0 );
 
-$conn = mysqli_connect($_SERVER['DB_HOST'];,$_SERVER['DB_USER'],$_SERVER['DB_PASSWORD'];);
+$conn = mysqli_connect($_SERVER['DB_HOST'],$_SERVER['DB_USER'],$_SERVER['DB_PASSWORD']);
 if($conn){
-mysqli_select_db($conn, $_SERVER['DB_NAME'];);
+mysqli_select_db($conn, $_SERVER['DB_NAME']);
 }else
 {
 die("Connection error: " . mysqli_connect_error());
