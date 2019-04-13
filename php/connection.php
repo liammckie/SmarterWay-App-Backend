48dbaaebd97d652ea0cf27dcca2f87a556a649bd
Daynis Olman <daynis@ditconsulting.com.au>
2019-04-13 19:46:08 +1000

    13 April - 7:45pm - Swap DB Name to Restored DB in app files

--- php/connection.php
+++ php/connection.php
@@ -1,8 +1,10 @@
 <?php
 $conn = mysqli_connect("wordpressdb.cfkyixhewlor.ap-southeast-2.rds.amazonaws.com","smart_mobile","&e3|MnuQYXQM");
 if($conn){
-	mysqli_select_db($conn, "smart_mobile_production");
+	mysqli_select_db($conn, "smart_production_10APR_restore");
 }else{
 	die("Connection error: " . mysqli_connect_error());
 }
 ?>
+
+
--- repslyapi/config.php
+++ repslyapi/config.php
@@ -3,7 +3,7 @@ set_time_limit ( 0 );
 
 $conn = mysqli_connect("wordpressdb.cfkyixhewlor.ap-southeast-2.rds.amazonaws.com","smart_mobile","&e3|MnuQYXQM");
 if($conn){
-	mysqli_select_db($conn, $_SERVER['DB_NAME']);
+	mysqli_select_db($conn, 'smart_production_10APR_restore');
 }else{
 	die("Connection error: " . mysqli_connect_error());
 }
