b2b933f34ac1342aedca280134eae64aed354c20
Daynis Olman <daynis@ditconsulting.com.au>
2019-04-14 13:11:57 +1000

    Change DB name from global variable to string

--- repslyapi/config.php
+++ repslyapi/config.php
@@ -3,7 +3,7 @@ set_time_limit ( 0 );
 
 $conn = mysqli_connect("162.241.218.43","vbeasyco_scs","vbeasyco_scs");
 if($conn){
-	mysqli_select_db($conn, $_SERVER['DB_NAME']);
+	mysqli_select_db($conn, "smart_mobile_production");
 }else{
 	die("Connection error: " . mysqli_connect_error());
 }
