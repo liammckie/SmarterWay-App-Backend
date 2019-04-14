f76f8103d74c1690dd6ce7bd6d21208bc645eedd
VB Easy-SCS <45724928+raj-smartclean@users.noreply.github.com>
2019-04-14 09:01:52 +0530

    Update connection.php
    push to master Thanks

--- php/connection.php
+++ php/connection.php
@@ -1,7 +1,7 @@
 <?php
 $conn = mysqli_connect("162.241.218.43","vbeasyco_scs","vbeasyco_scs");
 if($conn){
-	mysqli_select_db($conn, "smart_production_10APR_restore");
+	mysqli_select_db($conn, "vbeasyco_scs");
 }else{
 	die("Connection error: " . mysqli_connect_error());
 }
